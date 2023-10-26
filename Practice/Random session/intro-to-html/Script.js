42f194239f31189f37897125370853c96a627149
okun <okun2006@gmail.com>
2023-10-26 11:11:50 +0100

    first commit

---
+++ Practice/Random session/intro-to-html/Script.js
@@ -0,0 +1,23 @@
+switch('day'){
+  case 'monday':
+      console.log('monday')
+      break
+    case'tuesday':
+      console.log('tuesday')
+      break
+    case'wednesday':
+      console.log('wednesday')
+      break
+    case'thursday':
+      console.log('thursday')
+      break
+    case'friday':
+      console.log('friday')
+      break
+    case'saturday':
+      console.log('saturday')
+      break
+    case'sunday':
+      console.log('sunday');
+      break
+}
